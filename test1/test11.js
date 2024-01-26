9b15269dbe17660c398a5b55b1cf181b2cfc4ca3
Dream <2171204141@qq.com>
2024-01-26 10:44:38 +0800

    第十八次提交

--- test1/test11.js
+++ test1/test11.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-=======
-for(var j=0;j<5;j++){
-    console.log(j)
-}
->>>>>>> pyz
 for(var i=0;i<10;i++){
     console.log(i)
 }
 for(var w=10;w<20;w++){
     console.log(w)
 }
-<<<<<<< HEAD
 for(let j=0;j<5;j++){
     console.log(j)
 }
@@ -19,23 +12,13 @@ console.log("许墨")
 console.log("白起")
 console.log("周棋洛")
 console.log("凌肖")
-<<<<<<< HEAD
 
 console.log("nox")
-<<<<<<< HEAD
-console.log("很想死")
-=======
 
+console.log("很想死")
 console.log("想死")
->>>>>>> lxh
-=======
+
 console.log("不知道改什么")
-<<<<<<< HEAD
-=======
 console.log("123")
 console.log("234")
 console.log("345")
->>>>>>> pyz
-=======
->>>>>>> ba7d94a8d77a20493f4f25a1f09c88b873a698f2
->>>>>>> 8a29b5b95a4a2e0c182e9e4af7271647da870d22
