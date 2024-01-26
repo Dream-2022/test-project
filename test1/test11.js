ba7d94a8d77a20493f4f25a1f09c88b873a698f2
Dream <2171204141@qq.com>
2024-01-26 09:33:46 +0800

    第十一次提交

--- test1/test11.js
+++ test1/test11.js
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
-let j=0
 for(var i=0;i<10;i++){
     console.log(i)
 }
 for(var w=10;w<20;w++){
     console.log(w)
 }
-=======
+for(let j=0;j<5;j++){
+    console.log(j)
+}
 console.log("李泽言")
 console.log("许墨")
 console.log("白起")
 console.log("周棋洛")
 console.log("凌肖")
-
->>>>>>> b4c93698e9577d2c761a12a26afc7a10eeb5fbdd
+console.log("不知道改什么")
