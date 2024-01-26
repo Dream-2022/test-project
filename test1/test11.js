f42baf443de9a136268455d8fb48e71b69573286
Dream <2171204141@qq.com>
2024-01-26 10:37:42 +0800

    十六次提交

--- test1/test11.js
+++ test1/test11.js
@@ -1,12 +1,20 @@
-for(var j=0;j<5;j++){
-    console.log(j)
-}
 for(var i=0;i<10;i++){
     console.log(i)
 }
 for(var w=10;w<20;w++){
     console.log(w)
 }
+for(let j=0;j<5;j++){
+    console.log(j)
+}
+console.log("李泽言")
+console.log("许墨")
+console.log("白起")
+console.log("周棋洛")
+console.log("凌肖")
+console.log("不知道改什么")
 console.log("123")
 console.log("234")
 console.log("345")
+console.log("456")
+console.log(567)
