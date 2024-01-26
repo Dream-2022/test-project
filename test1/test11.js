8eace283e367210095ec74248140b5b2f8b2ebcd
Dream <2171204141@qq.com>
2024-01-26 10:19:18 +0800

    第十二次提交

--- test1/test11.js
+++ test1/test11.js
@@ -1,7 +1,12 @@
-let j=0
+for(var j=0;j<5;j++){
+    console.log(j)
+}
 for(var i=0;i<10;i++){
     console.log(i)
 }
 for(var w=10;w<20;w++){
     console.log(w)
 }
+console.log("123")
+console.log("234")
+console.log("345")
